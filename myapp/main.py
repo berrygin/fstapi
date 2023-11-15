ee9a9b074b2a437a14a187bc8ac5a22710e80c9e
AkiraWin <frog7bar@gmail.com>
2023-11-15 23:59:08 +0900

    xheadrs

--- myapp/main.py
+++ myapp/main.py
@@ -32,7 +32,7 @@ async def bkapp_page2(request: Request):
 pn.serve({'/app': createApp, '/app2': createApp2},
         port=5000, allow_websocket_origin=["127.0.0.1:8000"],
         address="0.0.0.0", 
-        # xheaders=True,
+        xheaders=True,
         show=False)
         # address="127.0.0.1", show=False)
 
