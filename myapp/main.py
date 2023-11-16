5e4fd10647a77084a3195457aaa3a3ffcbac9934
AkiraWin <frog7bar@gmail.com>
2023-11-16 22:56:10 +0900

    it is safe

--- myapp/main.py
+++ myapp/main.py
@@ -2,16 +2,29 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+from fastapi.middleware.cors import CORSMiddleware
 import panel as pn
 from bokeh.embed import server_document
 
-from myapp.sliders.pn_app import createApp
-from myapp.sliders2.pn_app2 import createApp2
+from sliders.pn_app import createApp
+from sliders2.pn_app2 import createApp2
 
 app = FastAPI()
-app.mount(path='/myapp/static', app=StaticFiles(directory='myapp/static'), name='static')
-templates = Jinja2Templates(directory='myapp/templates')
+origins = [
+    "https://localhost.tiangolo.com",
+    "http://127.0.0.1:5000",
+    "http://127.0.0.1:10000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount(path='/static', app=StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
 
 
 # @app.get('/', response_class=HTMLResponse)
@@ -21,16 +34,16 @@ templates = Jinja2Templates(directory='myapp/templates')
 
 @app.get("/")
 async def bkapp_page(request: Request):
-    script = server_document('http://0.0.0.0:5000/app')
+    script = server_document('http://127.0.0.1:5000/app')
     return templates.TemplateResponse("page.html", {"request": request, "script": script})
 
 @app.get("/app2")
 async def bkapp_page2(request: Request):
-    script = server_document('http://0.0.0.0:5000/app2')
+    script = server_document('http://127.0.0.1:5000/app2')
     return templates.TemplateResponse("page.html", {"request": request, "script": script})
 
 pn.serve({'/app': createApp, '/app2': createApp2},
-        port=5000, allow_websocket_origin=["0.0.0.0:10000"],
+        port=5000, allow_websocket_origin=["127.0.0.1:10000"],
         address="0.0.0.0", 
         # xheaders=True,
         show=False)
