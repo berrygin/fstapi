7e16d6fea50ad30a1e18df1842a0c3a218c91e8e
AkiraMac <frogcherry7@gmail.com>
2023-11-16 12:58:47 +0900

    rerere

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ o http://127.0.0.1:8000/
 x localhost:8000
 
 ```
-uvicorn main:app --host 0.0.0.0 --port $PORT
+uvicorn myapp.main:app --host 0.0.0.0 --port $PORT
 ```
--- myapp/main.py
+++ myapp/main.py
@@ -21,16 +21,16 @@ templates = Jinja2Templates(directory='myapp/templates')
 
 @app.get("/")
 async def bkapp_page(request: Request):
-    script = server_document('http://0.0.0.0:5000/app')
+    script = server_document('http://127.0.0.1:5000/app')
     return templates.TemplateResponse("page.html", {"request": request, "script": script})
 
 @app.get("/app2")
 async def bkapp_page2(request: Request):
-    script = server_document('http://0.0.0.0:5000/app2')
+    script = server_document('http://127.0.0.1:5000/app2')
     return templates.TemplateResponse("page.html", {"request": request, "script": script})
 
 pn.serve({'/app': createApp, '/app2': createApp2},
-        port=5000, allow_websocket_origin=["0.0.0.0:10000"],
+        port=5000, allow_websocket_origin=["127.0.0.1:8000"],
         address="0.0.0.0", 
         # xheaders=True,
         show=False)
